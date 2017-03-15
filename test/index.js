145d85e7b9ea8b5ed6ab94b2f2008f74890dd4db
Zachary <admin@hackzzila.com>
2017-03-15 01:55:57 -0400

    you trigger me

--- test/index.js
+++ test/index.js
@@ -1,9 +1,10 @@
 'use strict';
+
 const exec = require('child_process').exec;
 const path = require('path');
 
 exec(`ddoc -c ${__dirname}${path.sep}diamond.doc.json`, (err, stdout, stderr) => {
-  if(err) throw err;
+  if (err) throw err;
   console.log(stdout);
   console.error(stderr);
 });

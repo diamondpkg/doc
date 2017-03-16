ccbce843f343eb5561a826d9d92daffdf1350456
appellation <nelson.will@live.com>
2017-03-15 19:55:43 -0700

    start parsing betterer

--- src/parsers/stylesheet.js
+++ src/parsers/stylesheet.js
@@ -1,3 +1,34 @@
 'use strict';
 
-module.exports = () => {};
+class Parse {
+  constructor(iterator) {
+    this.i = iterator;
+    this.c = '';
+  }
+
+  start() {
+    while (this._iterate()) {
+      if (this.checkPrefix()) {
+        //
+      }
+    }
+  }
+
+  checkPrefix() {
+    let prev = '';
+    for (let i = 0; i < 3; i++) {
+      prev += this.c;
+      this._iterate();
+    }
+    return prev === '/**';
+  }
+
+  _iterate() {
+    return this.c = this.i.next().value;
+  }
+}
+
+module.exports = (content) => {
+  const parser = new Parse(content[Symbol.iterator]());
+  parser.start();
+};
